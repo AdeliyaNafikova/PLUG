7250368c7661702da235196a5c3b24ec2686a5a4
Adelia <Nafikovaadelia@gmail.com>
2023-07-04 20:34:41 +0300

    add new file example 005

---
+++ example005_ifelse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter a usage name");
+string username =Console.ReadLine();
+
+if(username.ToLower() == "masha") 
+{
+    Console.WriteLine("Horray, this is Masha!");
+
+}
+else 
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
